a788c1be2f03e428fe4b5722a457decfb7e7ab0d
Leonid Zharikov <leon.zharikov@gmail.com>
2021-05-08 22:16:12 +0300

    feat(zapkit): Add core builder

--- zapkit/core.go
+++ zapkit/core.go
@@ -5,3 +5,15 @@ import (
 )
 
 type CoreMakeFunc func(cfg Config) (zapcore.Core, error)
+
+type CoreBuildFunc func() CoreMakeFunc
+
+func CoreBuilder(initFuncs ...CoreBuildFunc) []CoreMakeFunc {
+	var cores []CoreMakeFunc
+	for _, initFunc := range initFuncs {
+		if f := initFunc(); f != nil {
+			cores = append(cores, f)
+		}
+	}
+	return cores
+}
